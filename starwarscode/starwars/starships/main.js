c561be52f3f5ec34ddad019a2976d27992262fb5
Sam LOuder <Loudersam96@gmail.com>
2020-03-11 16:02:01 -0600

    main.js update

--- starwarscode/starwars/starships/main.js
+++ starwarscode/starwars/starships/main.js
@@ -1,42 +1,40 @@
 import { starships } from '../data/starships.js'
-import { removeChildren} from '../utils.js'
+import { removeChildren, getLastNumber } from '../utils.js'
 
 const nav = document.querySelector('.nav')
 
-const navList = docuemnt.querySelector ('.navList')
+const navList = document.querySelector('.navList')
 
-const shipView = document.querySelector
+const shipView = document.querySelector('#main')
 
 function populateNav(starships) {
-    starships.forEach(starshio => {
+    starships.forEach(starship => {
         let shipAnchor = document.createElement('a')
         shipAnchor.href = '#'
-        let listItem = docuemnt.createElement('li')
+        let listItem = document.createElement('li')
         listItem.textContent = starship.name
 
         shipAnchor.addEventListener('click', event => {
+            // store the name of the list items clicked on 
             let shipName = event.target.textContent
-            populateShipView(shipName)
+            const foundShip = starships.find(ship => ship.name === shipName )
+            console.log(foundShip)
+            populateSHipView(foundShip)
         })
 
         shipAnchor.appendChild(listItem)
         navList.appendChild(shipAnchor)
-        
+
     })
     nav.appendChild(navList)
 }
 
-function populateShipView(data) {
+function populateShipView(shipData) {
     removeChildren(shipView)
-let shipTitle = document.createElement('h1')
-shipTitle.textContent.content = data
-shipView.appendChild(shipTitle)
-}
-
-function removeChildren(element) {
-    while(element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
+    let imageNum = getLastNumber(shipData.url)
+    let shipImage = document.createElement('img')
+    shipImage.src = `https://starwars-visualguide.com/assets.img.starships/${imageNum}.jpg`
+    shipView.appendChild(shipImage)   
 }
 
 populateNav(starships)
