6527768014b8d7b4354d268975860b3175fe3a7e
Sam LOuder <Loudersam96@gmail.com>
2020-03-24 11:15:35 -0600

    starfield background

--- starwarscode/personalportfolioproject/starships/main.js
+++ starwarscode/personalportfolioproject/starships/main.js
@@ -1,11 +1,11 @@
 import { starships } from '../data/starships.js'
-import { removeChildren, getLastNumber } from '../utils.js'
+import { removeChildren, getLastNumber, addStarField } from '../utils.js'
 
 const nav = document.querySelector('.nav')
 
 const navList = document.querySelector('.navList')
 
-const shipView = document.getElementById('main')
+const shipView = document.getElementById('.shipView')
 
 const modalDialog = document.querySelector('.modal')
 const modalButton = document.querySelector('.modal-close')
@@ -58,3 +58,5 @@ function populateShipView(shipData) {
  }
 
  populateNav(starships)
+
+ addStarField(document.body, )
--- starwarscode/personalportfolioproject/utils.js
+++ starwarscode/personalportfolioproject/utils.js
@@ -12,3 +12,17 @@ export function getLastNumber(url) {
     }
     return url.slice(start, end)
 }
+
+export function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i =0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('position', 'absolute')
+        star.style.setProperty('width', '5px')
+        star.style.setProperty('height', '5px')
+        star.style.setProperty('background-color', 'white')
+        star.style.left = `100px`
+        star.style.top = `100px`
+        element.appendChild(star)
+    }
+}
