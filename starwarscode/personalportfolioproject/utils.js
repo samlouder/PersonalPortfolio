8891723d7fcc34c3322da3c5fc8827090edccd0f
Sam LOuder <Loudersam96@gmail.com>
2020-03-26 10:27:39 -0600

    starfield

--- starwarscode/personalportfolioproject/utils.js
+++ starwarscode/personalportfolioproject/utils.js
@@ -18,11 +18,19 @@ export function addStarField(element, numStars) {
     for (let i =0; i < numStars; i++) {
         let star = document.createElement('div')
         star.style.setProperty('position', 'absolute')
-        star.style.setProperty('width', '5px')
-        star.style.setProperty('height', '5px')
+        star.style.setProperty('width', '1px')
+        star.style.setProperty('height', '1px')
         star.style.setProperty('background-color', 'white')
-        star.style.left = `100px`
-        star.style.top = `100px`
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
         element.appendChild(star)
     }
+}
+function getRandomPosition () {
+let x = document.body.scrollHeight
+let y = document.body.scrollWidth
+let randomY = Math.floor(Math.random() * y)
+let randomX = Math.floor(Math.random() * x)
+return [randomX, randomY]
 }
