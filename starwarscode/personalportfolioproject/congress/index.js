cd2b104d5dd047bf626673082599e3ef4afdbc30
Sam LOuder <Loudersam96@gmail.com>
2020-03-31 12:06:30 -0600

    mapped senators and pics

--- starwarscode/personalportfolioproject/congress/index.js
+++ starwarscode/personalportfolioproject/congress/index.js
@@ -5,32 +5,33 @@ import { senators } from '../data/senators.js'
 const container = document.querrySelector('.container')
 
 const filterSenators = (prop, value) => {
-    return senators.filter(senator => senator[prop] 
-    === value)
+    return senators.filter(senator => senator[prop] === value)
 }
-function populateContainer() {
-    senators.forEach(senator => {
-        let middleName = senator.middle_name ? ` $ 
-        {senator.middle_name} ` : ` `
-        let senPara = document.createElement('p')
-        senPara.textContent = 
-        `${senator.last_name}` 
-        container.appendChild(senPara)
-        let senFigure = document.createElement('figure')
-        let figImg = docuemnt.createElement('img')
-        let figCaption = document.createElement 
-        ('figcaption')
 
-        figImg.src = `https://www.govtrack.us/static/legislator-photos${senator.govtrack_id}-200px.jpeg`
-        figcaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
- 
+const mappedSenators = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+    }
+})
+
+function populateContainer(smallSenatorsArray) {
+    console.log(smallSenatorsArray) 
+    smallSenatorsArray.forEach(senator => {
+
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+        
         senFigure.appendChild(figImg)
-        senFigure.appendChild(figcaption)
+        senFigure.appendChild(figCaption)
         container.appendChild(senFigure)
     })
 }
 
-populateContainer()
-console.log(filterSenators('party', 'R'))
-console.log(filterSenators)('party', 'D')
-console.log(filterSenators)('party', 'ID')
+populateContainer(mappedSenators)
