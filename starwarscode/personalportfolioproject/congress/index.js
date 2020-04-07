7c1f40c9e7e3a9b6156ef52733eb497db7904124
Sam LOuder <Loudersam96@gmail.com>
2020-04-07 12:58:52 -0600

    worked on reduce functions

--- starwarscode/personalportfolioproject/congress/index.js
+++ starwarscode/personalportfolioproject/congress/index.js
@@ -15,7 +15,10 @@ function simplefiedSenattors(senatorArray) {
             id: senator.id,
             name: `${senator.first_name}${middleName}${senator.last_name}`,
             imgURL: `https://www.govtrackus/static/legislator-photos/${senator.gotrack_id}-200px.jpeg`,
-            seniority: parseInt(senator.seniority, 10)
+            seniority: parseInt(senator.seniority, 10),
+            votesWithPartyPct: senator.votes_with_party_pct,
+            party: senator.party,
+            missed_votes_pct: senator.missed_votes_pct
         }
     })
 }
@@ -39,13 +42,34 @@ function populateContainer(smallSenatorsArray) {
 const republicans = filterSenators('party', 'R')
 const democrats = filterSenators('party', 'D')
 
-const mostSeniority = simplifiedSenators(republicans).reduce(
+const mostSeniority = simplifiedSenators(democrats).reduce(
     (acc,senator) => {
-        //console.log(senator.seniority)
         return acc.seniority > senator.seniority ? acc : senator
     }
 )
 
+const mostMissedVotes = simplifiedSenators(senators).reduce((acc, senator) => {
+    return acc.missed_votes_pct > senator.missed_votes_pct ? acc : senator
+}
+)
+
+/* const loyalArray = simplifiedSenators(senators).map(senator => {
+    if (senator.votesWithPartyPct === 100) {
+        return senator
+    }
+}) */
+
+let loyalArray =[]
+
+const mostLoyal = simplifiedSenators(senators).reduce((acc, senator) => {
+    if (senator.votesWithPartyPct === 100) {
+        loyalArray.push(senator)
+    }
+    return acc.votesWithPartyPct > senator.votesWithPartyPct ? acc : senator
+})
+
 console.log(mostSeniority)
+console.log(loyalArray)
+console.log(mostMissedVotes)
 
 populateContainer(simplefiedSenators(republicans))
