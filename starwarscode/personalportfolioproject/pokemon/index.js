d7b4feee79ff0eb39d5fabf4593597d89f5177d8
Sam LOuder <Loudersam96@gmail.com>
2020-04-23 11:32:11 -0600

    pokemon update

--- starwarscode/personalportfolioproject/pokemon/index.js
+++ starwarscode/personalportfolioproject/pokemon/index.js
@@ -21,7 +21,7 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-  getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+  getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=50').then(
     (data) => {
       for (const pokemon of data.results) {
         getAPIData(pokemon.url).then(
